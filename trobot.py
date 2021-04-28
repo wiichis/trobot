e693b1ab7743e1785940e8dd1e5a766b60ea17ad
Wilson Montenegro <wilmontenegro@me.com>
2021-04-27 22:06:21 -0500

    Calculando el promedio

--- trobot.py
+++ trobot.py
@@ -1,6 +1,7 @@
 from bs4 import BeautifulSoup  
 import requests
 import schedule
+import numpy as np
 
 def bot_send_text(bot_message):
 
@@ -23,25 +24,30 @@ def btc_scraping():
     return format_result
 
 price_list = [] 
+
 def btc_price_list():
     price_list.append(btc_scraping())
-    #max_btc = max(price_list)
-    #min_btc = min(price_list)
-    #print(f'El precio del BTC es {price_list[-1]}, el max es {max_btc} y el min es {min_btc}')
+    #Quitando $ de la lista original y convirtiendo en float
+    global price_list_number
+    price_list_number = {i.lstrip("$") for i in price_list}
+    price_list_number = {i.replace(",","") for i in price_list_number}
+    price_list_number = {float(i) for i in price_list_number}
 
+    #print(f'El precio del BTC es {price_list[-1]}, el max es {max(price_list)}, el min es {min(price_list)} y el promedio es {btc_price_mean}')
+    #print(price_list_number)
     return 
 
 
 def report():
-    btc_price = f'El precio del BTC es {price_list[-1]}, el max es {max(price_list)} y el min es {min(price_list)}'
+    btc_price = f'El precio del BTC es {price_list[-1]}, el max es {max(price_list)}, el min es {min(price_list)} y el promedio es ${int(sum(price_list_number)/len(price_list_number))}'
     bot_send_text(btc_price)
 
 
 if __name__ == '__main__':
     
     #schedule.every().day.at("08:00").do(report)
-    schedule.every(60).minutes.do(report)
-    schedule.every(10).minutes.do(btc_price_list)
+    schedule.every(300).minutes.do(report)
+    schedule.every(45).minutes.do(btc_price_list)
     
 
     while True:
