d38caad060bbc3f47ca90eac06f17a95e561f57d
Wilson Montenegro <wilmontenegro@me.com>
2021-05-04 21:41:30 -0500

    Gurdando los precios enun archivo

--- trobot.py
+++ trobot.py
@@ -14,7 +14,6 @@ def bot_send_text(bot_message):
     return response
 
 
-
 def btc_scraping():
     url = requests.get('https://awebanalysis.com/es/coin-details/bitcoin/')
     soup = BeautifulSoup(url.content, 'html.parser')
@@ -23,31 +22,50 @@ def btc_scraping():
 
     return format_result
 
-price_list = [] 
+#Leyendo el archivo
+def read():
+    global price_list_filter
+    price_list = [] 
+    with open("./archivos/btc_price.csv", "r", encoding="utf-8") as f:
+        for i in f:
+            price_list.append(i)
+    price_list_filter = list(filter(lambda x: x != "\n", price_list))
 
 def btc_price_list():
-    price_list.append(btc_scraping())
+    price_list_filter.insert(0, btc_scraping())
+    if len(price_list_filter) > 800:
+        price_list_filter.pop()
     #Quitando $ de la lista original y convirtiendo en float
     global price_list_number
-    price_list_number = {i.lstrip("$") for i in price_list}
+    price_list_number = {i.lstrip("$") for i in price_list_filter}
     price_list_number = {i.replace(",","") for i in price_list_number}
     price_list_number = {float(i) for i in price_list_number}
+
 
     #print(f'El precio del BTC es {price_list[-1]}, el max es {max(price_list)}, el min es {min(price_list)} y el promedio es {btc_price_mean}')
     #print(price_list_number)
     return 
 
+#Guardando el archivo
+def write():
+    with open("./archivos/btc_price.csv", "w", encoding="utf-8") as f:
+        for i in price_list_filter:
+            f.write(i + "\n")
+            
+            
 
 def report():
-    btc_price = f'El precio del BTC es {price_list[-1]}, el max es {max(price_list)}, el min es {min(price_list)} y el promedio es ${int(sum(price_list_number)/len(price_list_number))}'
+    btc_price = f'El precio del BTC es {price_list_filter[-1]}, el max es {max(price_list_filter)}, el min es {min(price_list_filter)} y el promedio es ${int(sum(price_list_number)/len(price_list_number))}'
     bot_send_text(btc_price)
 
 
 if __name__ == '__main__':
     
     #schedule.every().day.at("08:00").do(report)
-    schedule.every(300).minutes.do(report)
+    schedule.every(290).minutes.do(report)
+    schedule.every(45).minutes.do(read)
     schedule.every(45).minutes.do(btc_price_list)
+    schedule.every(45).minutes.do(write)
     
 
     while True:
