e1b6426aa23eb6e8ce775e7c09ec2c915fb56d22
Wilson Montenegro <wilmontenegro@me.com>
2021-08-01 18:31:39 -0500

    Primera Version Alerta

--- trobot.py
+++ trobot.py
@@ -22,6 +22,11 @@ def btc_scraping():
 
     return format_result
 
+#Alertando cuando se rompe el techo o el piso
+def report_max_min(value_max_min):
+    btc_price_max_min = f'Alerta el valor {value_max_min} se acaba de romper el nuevo {value_max_min} es {price_list_filter[0]}'
+    bot_send_text(btc_price_max_min)
+
 #Leyendo el archivo
 def read():
     global price_list_filter
@@ -33,9 +38,18 @@ def read():
     price_list_filter = list(filter(lambda x: x != "\n", price_list))
 
 def btc_price_list():
-    price_list_filter.insert(0, btc_scraping())
+    global actual_value 
+    actual_value = btc_scraping()
+    price_list_filter.insert(0, actual_value)
     if len(price_list_filter) > 800:
         price_list_filter.pop()
+
+    #Alerta de cambio de Techo o piso
+    if actual_value == max(price_list_filter):
+        report_max_min('maximo')
+    elif actual_value == min(price_list_filter):
+        report_max_min('minimo')    
+
     #Quitando $ de la lista original y convirtiendo en float
     global price_list_number
     price_list_number = {i.lstrip("$") for i in price_list_filter}
@@ -64,7 +78,6 @@ def btc_price_day():
     price_list_day = price_list_filter[0:30]
     btc_report_day = f'Reporte Diario BTC\nAbrió con {price_list_day[14]}Cerró con {price_list_day[0]}\nEl max fue {max(price_list_day)}El min fue {min(price_list_day)}'
     bot_send_text(btc_report_day)
-
 
 if __name__ == '__main__':
     schedule.every().day.at("18:00").do(btc_price_day)
