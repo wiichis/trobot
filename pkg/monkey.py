9ff51524206f136e0dc4ae729d34916d2099f916
Wil Montenegro <wilmontenegro@me.com>
2023-02-08 18:37:57 -0500

    corrigiendo errores main

--- pkg/monkey.py
+++ pkg/monkey.py
@@ -114,7 +114,7 @@ def trading_result():
                         df_open['date'] = date
                         df_open['status'] = 'close'
                         df_open['result'] = 'perdida'
-                        df_open['result_USD'] = - (df_open['stop_lose'] * df_open['currency_amount']) - df_open['USD_Trade'] 
+                        df_open['result_USD'] = (df_open['stop_lose'] * df_open['currency_amount']) - df_open['USD_Trade'] 
                         df_open['USD_Total'] = df_open['result_USD'] + df_open['USD_Trade']
                         df_open['USD_Trade'] = 0
                         df = pd.concat([df, df_open])
