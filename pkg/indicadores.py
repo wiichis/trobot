dc113c8e80aa63e4f3d0a23fbdb553bb94be07dc
Wil Montenegro <wilmontenegro@me.com>
2023-02-28 22:14:02 -0500

    Modificando profit y stop lose a 0.9

--- pkg/indicadores.py
+++ pkg/indicadores.py
@@ -56,13 +56,13 @@ def ema_alert(currencie):
     envelope_inferior = df_filterd['envelope_inferior'].iloc[-1]
        
     if type == 'LONG':
-        stop_lose = price_last * 0.9967
-        profit = price_last * 1.01
+        stop_lose = price_last * 0.997
+        profit = price_last * 1.009
         tipo = '=== Alerta de LONG ==='
         return price_last, stop_lose, profit, tipo, envelope_superior, envelope_inferior
     elif type == 'SHORT':
-        stop_lose = price_last * 1.0033
-        profit = price_last * 0.99
+        stop_lose = price_last * 1.003
+        profit = price_last * 0.991
         tipo = '=== Alerta de SHORT ==='
         return price_last, stop_lose, profit, tipo, envelope_superior, envelope_inferior
     else:
