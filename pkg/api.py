92a04ffc20e8ad8236b1af971df9b2fda7bcbbf6
Wil Montenegro <wilmontenegro@me.com>
2023-08-28 16:48:19 -0500

    optimizacion consulta de precios con la API

--- pkg/api.py
+++ pkg/api.py
@@ -4,48 +4,54 @@ import requests
 import pandas as pd
 import time
 from datetime import datetime
-
+from concurrent.futures import ThreadPoolExecutor
 
 def currencies_list():
-  currencies = ['DYDX-USDT','ETH-USDT','BTC-USDT','SOL-USDT','TRX-USDT','CFX-USDT','OP-USDT','XRP-USDT','LDO-USDT','FIL-USDT']
-  return currencies
+    return ['DYDX-USDT','ETH-USDT','BTC-USDT','SOL-USDT','TRX-USDT','CFX-USDT','OP-USDT','XRP-USDT','LDO-USDT','FIL-USDT']
+
+def fetch_price(symbol):
+    try:
+        json_str = pkg.bingx.last_price_trading_par(symbol)
+        data = json.loads(json_str)
+        return {
+            'symbol': data["data"]["symbol"],
+            'price': data["data"]["price"],
+            'date': datetime.now()
+        }
+    except Exception as e:
+        print(f"Error al obtener el precio para {symbol}: {e}")
+        return None
 
 def price_bingx():
     try:
         currencies = currencies_list()
         data_list = []
-        for currencie in currencies:
-            json_str = pkg.bingx.last_price_trading_par(currencie)
 
-            # Analiza la cadena JSON
-            data = json.loads(json_str)
+        with ThreadPoolExecutor() as executor:
+            results = executor.map(fetch_price, currencies)
 
-            # Accede a los valores del objeto JSON
-            symbol = data["data"]["symbol"]
-            price = data["data"]["price"]
-            date = datetime.now()
-
-            # Agrega los valores a una lista de datos
-            data_list.append({'symbol': symbol, 'price': price, 'date': date})
+        for result in results:
+            if result is not None:
+                data_list.append(result)
         
-        # Crea un DataFrame a partir de la lista de datos
-        df = pd.DataFrame(data_list)
+        new_df = pd.DataFrame(data_list)
 
-        # Guarda el DataFrame en un archivo CSV
-        df_file = pd.read_csv('./archivos/cripto_price.csv')
-        df_new = pd.concat([df_file, df], ignore_index=True)
-        df_month = df_new.iloc[-10000:]
+        try:
+            existing_df = pd.read_csv('./archivos/cripto_price.csv')
+        except FileNotFoundError:
+            existing_df = pd.DataFrame(columns=['symbol', 'price', 'date'])
+
+        concatenated_df = pd.concat([existing_df, new_df], ignore_index=True)
+        df_month = concatenated_df.iloc[-10000:]
         df_month.to_csv('./archivos/cripto_price.csv', index=False)
         
     except requests.exceptions.ConnectionError as e:
-        print("Error de conexión: ", e)
+        print(f"Error de conexión: {e}")
         print("Reintentando la solicitud en 1 minuto...")
-        # Espera 1 minuto antes de volver a ejecutar la función
         time.sleep(60)
         price_bingx()
     except Exception as e:
-        print("Ocurrió un error: ", e)
-        # Realiza alguna acción adicional para manejar otros tipos de errores
+        print(f"Ocurrió un error: {e}")
 
 
 
