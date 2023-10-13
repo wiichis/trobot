56e16388860e2483a0b76253c3628ed23e0feddf
Wil Montenegro <wilmontenegro@me.com>
2023-10-12 23:32:14 -0500

    Incluyendo Logica Vela Larga

--- pkg/indicadores.py
+++ pkg/indicadores.py
@@ -3,23 +3,41 @@ import numpy as np
 from datetime import datetime
 import talib
 
+#Calculo de ATR de Velas
+def calculate_atr(data, period=14):
+    high = data['high']
+    low = data['low']
+    close = data['close']
+
+    tr = pd.concat([high - low, abs(high - close.shift()), abs(low - close.shift())], axis=1).max(axis=1)
+    atr = tr.rolling(period).mean()
+
+    return atr
+
+#Identificar Velas Largas
+def is_long_candle(data, multiplier=2):   #Multiplier es el tamaño de las velas
+    candle_range = data['high'] - data['low']
+    atr = calculate_atr(data)
+    return candle_range > multiplier * atr
+
+
 
 def emas_indicator():
     # Importar datos de precios
-    df = pd.read_csv('./archivos/cripto_price.csv')
+    df = pd.read_csv('./archivos/cripto_price.csv', parse_dates=["date"])
     df = df.iloc[-20000:] 
    
     grouped = df.groupby('symbol')
-  
+
     # Calcular el EMA de período 50 y el EMA de período 21 para cada grupo
     ema50 = grouped['price'].transform(lambda x: talib.EMA(x, timeperiod=50))
     ema21 = grouped['price'].transform(lambda x: talib.EMA(x, timeperiod=21))
-    df['ema50'] = ema50
-    df['ema21'] = ema21
+    df['ema50'] = round(ema50, 4)
+    df['ema21'] = round(ema21, 4)
 
     # Calcular el RSI de período 14 para cada grupo
     rsi = grouped['price'].transform(lambda x: talib.RSI(x, timeperiod=14))
-    df['rsi'] = rsi
+    df['rsi'] = round(rsi, 4)
 
      # Calcular la media móvil exponencial
     ancho_banda = 5
@@ -33,16 +51,32 @@ def emas_indicator():
     envelope_superior = smoothing + 2 * residuos
     envelope_inferior = smoothing - 2 * residuos
     price = df['price']
-    df['envelope_superior'] = envelope_superior
-    df['envelope_inferior'] = envelope_inferior
+    df['envelope_superior'] = round(envelope_superior, 4)
+    df['envelope_inferior'] = round(envelope_inferior,4 )
 
+        # Creamos una columna que identifica a qué intervalo de 5 minutos pertenece cada fila
+    df["time_bin"] = df["date"].dt.floor('15T')
+
+    # Agrupamos por símbolo y time_bin y generamos los valores para cada vela
+    grouped_velas = df.groupby(['symbol', 'time_bin'])
+    df = df.join(grouped_velas['price'].agg(open='first', high='max', low='min', close='last'), on=['symbol', 'time_bin'])
+
+    ohlc = grouped_velas['price'].agg(open='first', high='max', low='min', close='last')
+    ohlc.reset_index(inplace=True)
+
+     # Calculamos el ATR y si la vela es larga para cada grupo y lo añadimos al DataFrame ohlc
+    ohlc['ATR'] = round(ohlc.groupby('symbol').apply(lambda x: calculate_atr(x)).reset_index(level=0, drop=True),3)
+    ohlc['IsLongCandle'] = ohlc.groupby('symbol').apply(lambda x: is_long_candle(x)).reset_index(level=0, drop=True)
+
+    # Merge ohlc de vuelta con df para agregar las nuevas columnas calculadas
+    merged_df = pd.merge(df, ohlc, how='left', on=['symbol', 'time_bin'], suffixes=('', '_ohlc'))
 
     #Calcular la columna 'type' utilizando los valores de EMA y RSI para cada fila
-    df['type'] = 'NONE'
-    df.loc[(ema50 > ema21) & (rsi < 30) & (envelope_inferior >= price), 'type'] = 'LONG'
-    df.loc[(ema50 < ema21) & (rsi > 70) & (envelope_superior <= price),'type'] = 'SHORT'
+    merged_df['type'] = 'NONE'
+    merged_df.loc[(ema50 > ema21) & (rsi < 30) & (envelope_inferior >= price) & (merged_df['IsLongCandle'] == 'False' ), 'type'] = 'LONG'
+    merged_df.loc[(ema50 < ema21) & (rsi > 70) & (envelope_superior <= price) & (merged_df['IsLongCandle'] == 'False' ),'type'] = 'SHORT'
             
-    cruce_emas = df.groupby('symbol').tail(20).reset_index()
+    cruce_emas = merged_df.groupby('symbol').tail(20).reset_index()
     cruce_emas = cruce_emas.sort_values(['symbol', 'date'])
     cruce_emas.to_csv('./archivos/indicadores.csv', index=False)
 
--- pkg/monkey_bx.py
+++ pkg/monkey_bx.py
@@ -202,10 +202,10 @@ def colocando_ordenes():
 
 def colocando_TK_SL():
     #Configuracion SL TP
-    long_stop_lose = 0.998333
-    long_profit = 1.0058
-    short_stop_lose = 1.001667
-    short_profit = 0.9942
+    long_stop_lose = 0.9985
+    long_profit = 1.0048
+    short_stop_lose = 1.0015
+    short_profit = 0.9952
 
     #obteniendo posiciones sin SL o TP
     df_posiciones = pd.read_csv('./archivos/position_id_register.csv')
